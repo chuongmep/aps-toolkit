76559daa931a66f0e5d7d3088138bed62e3c519d
chuongmep <31106432+chuongmep@users.noreply.github.com>
2024-03-03 15:50:31 +0800

    fix data recursive null

--- APSToolkitPython/src/aps_toolkit/ProDbReaderRevit.py
+++ APSToolkitPython/src/aps_toolkit/ProDbReaderRevit.py
@@ -47,9 +47,6 @@ class PropDbReaderRevit(PropReader):
             category = category[5:].strip()
         category_id = [key for key, value in categories.items() if value == category]
         dataframe = self._get_recursive_ids(category_id)
-        # remove all row have all values is null, ignore dbId and external_id columns
-        dataframe = dataframe.dropna(how='all',
-                                     subset=[col for col in dataframe.columns if col not in ['dbId', 'external_id']])
         return dataframe
 
     def get_data_by_categories(self, categories: List[str]) -> pd.DataFrame:
@@ -95,6 +92,8 @@ class PropDbReaderRevit(PropReader):
             dataframe = pd.concat([dataframe, singleDF], ignore_index=True)
             ids = self.get_children(child_id)
             dataframe = pd.concat([dataframe, self._get_recursive_ids(ids)], ignore_index=True)  # Recurse for children
+        dataframe = dataframe.dropna(how='all',
+                                     subset=[col for col in dataframe.columns if col not in ['dbId', 'external_id']])
         return dataframe
 
     def _get_recursive_ids_prams(self, childs: List[int], params: List[str]) -> pd.DataFrame:
