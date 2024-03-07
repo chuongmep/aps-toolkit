49c965ced75e5114e9c6cb5c93dd67fe0ff4a8ef
Chuong Ho <chuongpqvn@gmail.com>
2024-03-07 13:10:37 +0800

    update hook url discord

--- APSToolkit/Utils/DiscordWebhook.cs
+++ APSToolkit/Utils/DiscordWebhook.cs
@@ -6,16 +6,13 @@ namespace APSToolkit.Utils;
 
 public class DiscordWebhook
 {
-    private string WebHookUrl { get; set; } =
-        "https://discordapp.com/api/webhooks/1182501348734943272/2xI-zV7JGQjatZgBC8w1-SKTZlHxg5-hnvDfza8qi5OK-842K2RuZRtbAE2A7zH5VCld";
     public string username { get; set; }
     public string avatar_url { get; set; }
     public string content { get; set; }
 
-    public void SendReport(string title, string content)
+    public void SendReport(string webHookUrl,string title, string content)
     {
-        string url = WebHookUrl;
-        var client = new RestClient(url);
+        var client = new RestClient(webHookUrl);
         var request = new RestRequest() {Method = Method.Post};
         DiscordWebhook discordWebhook = new DiscordWebhook()
         {
@@ -25,6 +22,5 @@ public class DiscordWebhook
         // add raw content json with {content: "{content}"}'
         request.AddJsonBody(discordWebhook);
         client.ExecuteAsync(request);
-        Console.WriteLine("Done report to discord");
     }
 }
