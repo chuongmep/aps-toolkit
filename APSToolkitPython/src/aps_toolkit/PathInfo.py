487934c6800570308fc5ae563673df7ddb9223db
chuongmep <31106432+chuongmep@users.noreply.github.com>
2024-03-15 21:19:45 +0800

    sorry  dev :(, I don't want change

--- APSToolkitPython/src/aps_toolkit/PathInfo.py
+++ APSToolkitPython/src/aps_toolkit/PathInfo.py
@@ -1,3 +1,19 @@
+"""
+Copyright (C) 2024  chuongmep.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 class PathInfo:
     def __init__(self, root_file_name=None, base_path=None, local_path=None, urn=None):
         self.root_file_name = root_file_name
