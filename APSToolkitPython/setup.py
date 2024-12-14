9b3a50752ac2d8656ba2934e25acab81ffd46153
chuongmep <31106432+chuongmep@users.noreply.github.com>
2024-12-14 16:16:55 +0800

    Update setup.py

--- APSToolkitPython/setup.py
+++ APSToolkitPython/setup.py
@@ -6,7 +6,7 @@ with open("Readme.md") as f:
 
 setuptools.setup(
     name="aps_toolkit",
-    version="1.1.6",
+    version="1.1.7",
     author="chuong mep",
     author_email="chuongpqvn@gmail.com",
     description="A Toolkit Autodesk Platform Services for Python",
